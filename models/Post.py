2bee351de435b8197b36ce900dba9085c5a947d1
Alex de Roos <100049383+Alex-de-Roos@users.noreply.github.com>
2025-02-19 19:19:10 +0100

    added creation_date to Post

--- models/Post.py
+++ models/Post.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from typing import List
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped
 from setup import db
 from .Like import user_post_likes
+
 
 class Post(db.Model):
     __tablename__ = "posts"
@@ -9,6 +12,7 @@ class Post(db.Model):
     id: Mapped[int] = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Title: Mapped[str] = db.Column(db.String(50))
     Content: Mapped[str] = db.Column(db.Text)
+    creation_date: Mapped[datetime] = db.Column(db.DateTime, server_default=func.now())
 
     UserId: Mapped[int] = db.Column(
         db.Integer, db.ForeignKey("users.id"), nullable=False
