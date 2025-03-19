9038b0b76cff55c83a21327f63673cc45d4de3c6
Jeremy-Verweij <aj.verweij@st.hanze.nl>
2025-03-19 11:43:28 +0100

    fixed formatting issues when adding in space characters

--- utils/format_content.py
+++ utils/format_content.py
@@ -4,15 +4,15 @@ import re
 def url_to_html(match):
         url = match.group(0)
         if re.search(r'\.(png|jpg|jpeg|gif|bmp|webp)$', url, re.IGNORECASE):
-            return f'<img style="max-width: 200px; max-height: 200px;" src="{url.replace(" ", "")}" alt="{url}">'
+            return f'<img style="max-width: 200px; max-height: 200px;" src="{url}" alt="{url}">'
         else:
-            return f'<a href="{url.replace("&nbsp;", "")}">{url.replace("&nbsp;", "")}</a>'
+            return f'<a href="{url}">{url}</a>'
     
 def format_content(content:str):
     content = content.replace("&", "&amp;")
     content = content.replace(">", "&gt;")
     content = content.replace("<", "&lt;")
-    content = content.replace(" ", "&nbsp;")
+    content = content.replace("  ", " &nbsp;")
     content = content.replace("\n", "<br>")
     content = re.sub(r'##(.*?)##', r'<strong>\1</strong>', content)
     content = re.sub(r'@(\w+)', "<a href='user_name/" + r'\1' + "'>@" + r'\1' + "</a>", content)
