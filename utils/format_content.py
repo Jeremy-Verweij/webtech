6d5136ec1c5cd4495409743fef21dfe7aefe7314
Jeremy-Verweij <aj.verweij@st.hanze.nl>
2025-03-19 11:33:44 +0100

    added formatting for strong tags(using ## the user creates a strong tag)

--- utils/format_content.py
+++ utils/format_content.py
@@ -4,9 +4,9 @@ import re
 def url_to_html(match):
         url = match.group(0)
         if re.search(r'\.(png|jpg|jpeg|gif|bmp|webp)$', url, re.IGNORECASE):
-            return f'<img style="max-width: 200px; max-height: 200px;" src="{url}" alt="{url}">'
+            return f'<img style="max-width: 200px; max-height: 200px;" src="{url.replace(" ", "")}" alt="{url}">'
         else:
-            return f'<a href="{url}">{url}</a>'
+            return f'<a href="{url.replace("&nbsp;", "")}">{url.replace("&nbsp;", "")}</a>'
     
 def format_content(content:str):
     content = content.replace("&", "&amp;")
@@ -14,6 +14,7 @@ def format_content(content:str):
     content = content.replace("<", "&lt;")
     content = content.replace(" ", "&nbsp;")
     content = content.replace("\n", "<br>")
+    content = re.sub(r'##(.*?)##', r'<strong>\1</strong>', content)
     content = re.sub(r'@(\w+)', "<a href='user_name/" + r'\1' + "'>@" + r'\1' + "</a>", content)
     content = re.sub(r'https?://[a-zA-Z0-9\-._~:/?#[\]@!$&\'()*+,;=.]+', url_to_html, content)
     
